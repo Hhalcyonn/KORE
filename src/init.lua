522d090ae20a876d14da4dec71f81c53a7222aac
Hhalcyonn <ahsanhami9@gmail.com>
2026-09-02 11:47:07 +0700

    Add src/init.lua as central framework entry point

---
+++ src/init.lua
@@ -0,0 +1,15 @@
+-- L2D-BootlegSet/src/init.lua
+-- Central entry point for the L2D-BootlegSet framework
+-- This file exports all the main modules for easy access
+
+return {
+    ECS = require("src/EntityComponentSystem"),
+    AssetsSystem = require("src/AssetsSystem"),
+    WorldSystem = require("src/WorldComponentSystem"),
+    RenderSystem = require("src/RenderComponentSystem"),
+    Console = require("src/Console"),
+    Physics = require("src/PhysicsComponentSystem"),
+    UISystem = require("src/UISystem"),
+    Prefabs = require("src/Prefabs"),
+    Utils = require("src/utils"),
+}
